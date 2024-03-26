16269d7de3ea66b1f7640bb8de1e3d6a4f58fb8b
liubin <liubin361163@163.com>
2024-03-26 18:18:46 +0800

    增加头文件

---
+++ ios/DAAudioVideo.framework/Headers/DACustomCacheQueue.h
@@ -0,0 +1,29 @@
+//
+//  DACustomQueue.h
+//  DAAudioVideo
+//
+//  Created by Darren on 2020/6/2.
+//  Copyright © 2020 Darren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DAFrame.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DACustomCacheQueue <__covariant ObjectType> : NSObject
+
+@property (nonatomic, readonly) BOOL isEnabled; // 缓存是否已经准备好
+
+- (void)enableCache:(BOOL)enable;
+- (void)clear;
+- (NSInteger)size;
+- (BOOL)isEmpty;
+- (void)enQueueWithObject:(id)object;
+- (ObjectType)dequeue;
+- (ObjectType)front;
+- (void)setMaxCacheSize:(NSUInteger)maxCacheSize;
+- (void)setBufferingTime:(NSTimeInterval)time;
+
+@end
+
+NS_ASSUME_NONNULL_END

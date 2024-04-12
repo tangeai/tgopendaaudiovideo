43f2e0f62aadb6fffea98f62d421a3e4fe522a66
liubin <liubin361163@163.com>
2024-04-12 15:45:11 +0800

    1.0.9修复缓冲问题

--- ios/DAAudioVideo.framework/Headers/DACustomCacheQueue.h
+++ ios/DAAudioVideo.framework/Headers/DACustomCacheQueue.h
@@ -12,16 +12,21 @@ NS_ASSUME_NONNULL_BEGIN
 @interface DACustomCacheQueue <__covariant ObjectType> : NSObject
 
 @property (nonatomic, readonly) BOOL isEnabled; // 缓存是否已经准备好
+@property (nonatomic, assign) double wasteTime;
+@property (nonatomic, assign) NSUInteger count;
+@property (nonatomic, assign) double speed;
+@property (nonatomic) NSTimeInterval currentBufferedDuration; // 当前缓冲的总时长
 
-- (void)enableCache:(BOOL)enable;
 - (void)clear;
 - (NSInteger)size;
 - (BOOL)isEmpty;
 - (void)enQueueWithObject:(id)object;
 - (ObjectType)dequeue;
+//- (DAFrame *)dequeueList;
 - (ObjectType)front;
 - (void)setMaxCacheSize:(NSUInteger)maxCacheSize;
 - (void)setBufferingTime:(NSTimeInterval)time;
+- (double)getVideoSpeed;
 
 @end
 

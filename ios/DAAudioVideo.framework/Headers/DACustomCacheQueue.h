089cacb2f8c87ab6f569403b332e51ec025d836f
liubin <liubin361163@163.com>
2024-05-17 15:21:57 +0800

    修改版本

--- ios/DAAudioVideo.framework/Headers/DACustomCacheQueue.h
+++ ios/DAAudioVideo.framework/Headers/DACustomCacheQueue.h
@@ -12,21 +12,17 @@ NS_ASSUME_NONNULL_BEGIN
 @interface DACustomCacheQueue <__covariant ObjectType> : NSObject
 
 @property (nonatomic, readonly) BOOL isEnabled; // 缓存是否已经准备好
-@property (nonatomic, assign) double wasteTime;
-@property (nonatomic, assign) NSUInteger count;
-@property (nonatomic, assign) double speed;
-@property (nonatomic) NSTimeInterval currentBufferedDuration; // 当前缓冲的总时长
+@property (nonatomic, readonly) CGFloat currentBufferTime; // 当前缓冲的总时长
+@property (nonatomic, readonly) CGFloat currentBufferSize;
 
 - (void)clear;
 - (NSInteger)size;
 - (BOOL)isEmpty;
 - (void)enQueueWithObject:(id)object;
 - (ObjectType)dequeue;
-//- (DAFrame *)dequeueList;
 - (ObjectType)front;
 - (void)setMaxCacheSize:(NSUInteger)maxCacheSize;
 - (void)setBufferingTime:(NSTimeInterval)time;
-- (double)getVideoSpeed;
 
 @end
 
--- ios/DAAudioVideo.framework/Headers/DASaveWavFileFormatTool.h
+++ ios/DAAudioVideo.framework/Headers/DASaveWavFileFormatTool.h
@@ -12,8 +12,20 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface DASaveWavFileFormatTool : NSObject
 
+/// 将pcm文件转换成g711a格式
+/// - Parameters:
+///   - pcmPath: pcm文件路径
+///   - g711aPath: g711a文件路径
 + (BOOL)transformPCMFileToG711AFile:(NSString *)pcmPath g711aPath:(NSString *)g711aPath;
 
+/// 文件转化为wav文件
+/// - Parameters:
+///   - audioPath: 需要转换的文件路径（例如g711a）
+///   - filePath: wav文件路径
+///   - channels: 通道数（以获取支持的报警音频文件格式信息指令为准）
+///   - sampleRate: 采样率（以获取支持的报警音频文件格式信息指令为准）
+///   - bitsPerSample: 码率（以获取支持的报警音频文件格式信息指令为准）
+///   - codeID: 编码（以获取支持的报警音频文件格式信息指令为准）
 + (BOOL)saveWavFormatWithAudioPath:(NSString *)audioPath filePath:(NSString *)filePath channels:(unsigned int)channels sampleRate:(unsigned int)sampleRate bitsPerSample:(unsigned int)bitsPerSample codeID:(DAMediaCodeID)codeID;
 
 @end

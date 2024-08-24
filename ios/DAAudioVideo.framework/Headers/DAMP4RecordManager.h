1d86ea101d07608f49109b7b62f2bb37171dc41f
liubin <liubin361163@163.com>
2024-08-24 15:38:46 +0800

    修改分辨率内容

--- ios/DAAudioVideo.framework/Headers/DAMP4RecordManager.h
+++ ios/DAAudioVideo.framework/Headers/DAMP4RecordManager.h
@@ -19,6 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, copy) void(^stopRecordCallback)(NSString *filePath);
 
 - (void)startRecordWithFileName:(NSString *)fileName;
+- (void)startRecordWithFileName:(NSString *)fileName widthPixel:(int)width heightPixel:(int)height;
 - (void)stopRecordWithVideoPath:(nullable void(^)(NSString *filePath))callBack;
 
 - (BOOL)writeVideoData:(DACameraP2PVideoData *)videoData;
